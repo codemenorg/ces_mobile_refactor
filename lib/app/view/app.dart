8a14451e84183820f37a789f78c7b6e33c4376d6
MG Rabbi <golamrabbi3@gmail.com>
2024-02-08 16:53:25 +0600

    implement app ui

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,6 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:ces/app/routes/routes.dart';
 import 'package:ces/l10n/l10n.dart';
-import 'package:ces/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -9,12 +10,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
+      themeMode: ThemeMode.light,
+      theme: const AppTheme().themeData,
+      darkTheme: const AppDarkTheme().themeData,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       onGenerateRoute: Routes.onGenerateRoute,
